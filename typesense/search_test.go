58660ca6fce0de8f03717dce0e449d89960a0615
v-byte-cpu <65545655+v-byte-cpu@users.noreply.github.com>
2020-12-20 00:10:43 +0300

    Add Search API

---
+++ typesense/search_test.go
@@ -0,0 +1,176 @@
+package typesense
+
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+	"github.com/v-byte-cpu/typesense-go/typesense/api"
+	"github.com/v-byte-cpu/typesense-go/typesense/api/mocks"
+	"github.com/v-byte-cpu/typesense-go/typesense/api/pointer"
+)
+
+func newSearchParams() *api.SearchCollectionParams {
+	return &api.SearchCollectionParams{
+		Q:              "text",
+		QueryBy:        []string{"company_name"},
+		MaxHits:        pointer.Interface("all"),
+		Prefix:         pointer.True(),
+		FilterBy:       pointer.String("num_employees:=100"),
+		SortBy:         &([]string{"num_employees:desc"}),
+		FacetBy:        &([]string{"year_started"}),
+		MaxFacetValues: pointer.Int(10),
+		FacetQuery:     pointer.String("facetQuery"),
+		NumTypos:       pointer.Int(2),
+		Page:           pointer.Int(1),
+		PerPage:        pointer.Int(10),
+		GroupBy:        &([]string{"country"}),
+		GroupLimit:     pointer.Int(3),
+		IncludeFields:  &([]string{"company_name"}),
+	}
+}
+
+func newSearchResult() *api.SearchResult {
+	return &api.SearchResult{
+		Found:       1,
+		TookMs:      1,
+		FacetCounts: []int{},
+		Hits: []api.SearchResultHit{
+			api.SearchResultHit{
+				Highlights: []api.SearchHighlight{
+					api.SearchHighlight{
+						Field:         "company_name",
+						Snippet:       "<mark>Stark</mark> Industries",
+						MatchedTokens: []string{"Stark"},
+					},
+				},
+				Document: map[string]interface{}{
+					"id":            "124",
+					"company_name":  "Stark Industries",
+					"num_employees": float64(5215),
+					"country":       "USA",
+				},
+			},
+		},
+	}
+}
+
+func TestSearchResultDeserialization(t *testing.T) {
+	inputJSON := `{
+		"facet_counts": [],
+		"found": 1,
+		"took_ms": 1,
+		"hits": [
+		  {
+			"highlights": [
+			  {
+				"field": "company_name",
+				"snippet": "<mark>Stark</mark> Industries",
+				"matched_tokens": ["Stark"]
+			  }
+			],
+			"document": {
+			  "id": "124",
+			  "company_name": "Stark Industries",
+			  "num_employees": 5215,
+			  "country": "USA"
+			}
+		  }
+		]
+	  }`
+	expected := &api.SearchResult{
+		Found:       1,
+		TookMs:      1,
+		FacetCounts: []int{},
+		Hits: []api.SearchResultHit{
+			api.SearchResultHit{
+				Highlights: []api.SearchHighlight{
+					api.SearchHighlight{
+						Field:         "company_name",
+						Snippet:       "<mark>Stark</mark> Industries",
+						MatchedTokens: []string{"Stark"},
+					},
+				},
+				Document: map[string]interface{}{
+					"id":            "124",
+					"company_name":  "Stark Industries",
+					"num_employees": float64(5215),
+					"country":       "USA",
+				},
+			},
+		},
+	}
+
+	result := &api.SearchResult{}
+	err := json.Unmarshal([]byte(inputJSON), &result)
+	assert.Nil(t, err)
+	assert.Equal(t, expected, result)
+}
+
+func TestCollectionSearch(t *testing.T) {
+	expectedParams := newSearchParams()
+	expectedResult := newSearchResult()
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mockAPIClient := mocks.NewMockClientWithResponsesInterface(ctrl)
+	mockedResult := newSearchResult()
+
+	mockAPIClient.EXPECT().
+		SearchCollectionWithResponse(gomock.Not(gomock.Nil()), "companies", expectedParams).
+		Return(&api.SearchCollectionResponse{
+			JSON200: mockedResult,
+		}, nil).
+		Times(1)
+
+	client := NewClient(WithAPIClient(mockAPIClient))
+	params := newSearchParams()
+	result, err := client.Collection("companies").Documents().Search(params)
+
+	assert.Nil(t, err)
+	assert.Equal(t, expectedResult, result)
+}
+
+func TestCollectionSearchOnApiClientErrorReturnsError(t *testing.T) {
+	expectedParams := newSearchParams()
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mockAPIClient := mocks.NewMockClientWithResponsesInterface(ctrl)
+
+	mockAPIClient.EXPECT().
+		SearchCollectionWithResponse(gomock.Not(gomock.Nil()), "companies", expectedParams).
+		Return(nil, errors.New("failed request")).
+		Times(1)
+
+	client := NewClient(WithAPIClient(mockAPIClient))
+	params := newSearchParams()
+	_, err := client.Collection("companies").Documents().Search(params)
+	assert.NotNil(t, err)
+}
+
+func TestCollectionSearchOnHttpStatusErrorCodeReturnsError(t *testing.T) {
+	expectedParams := newSearchParams()
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mockAPIClient := mocks.NewMockClientWithResponsesInterface(ctrl)
+
+	mockAPIClient.EXPECT().
+		SearchCollectionWithResponse(gomock.Not(gomock.Nil()), "companies", expectedParams).
+		Return(&api.SearchCollectionResponse{
+			HTTPResponse: &http.Response{
+				StatusCode: 500,
+			},
+			Body: []byte("Internal Server error"),
+		}, nil).
+		Times(1)
+
+	client := NewClient(WithAPIClient(mockAPIClient))
+	params := newSearchParams()
+	_, err := client.Collection("companies").Documents().Search(params)
+	assert.NotNil(t, err)
+}
