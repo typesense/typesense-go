22583ade642567e6ae0ebd07591207b21a5d9c23
khalilsarwari <khalil.sarwari@berkeley.edu>
2021-10-12 12:32:15 -0700

    add range filter test from #63

--- typesense/test/search_test.go
+++ typesense/test/search_test.go
@@ -56,3 +56,45 @@ func TestCollectionSearch(t *testing.T) {
 
 	require.Equal(t, expectedDocs, docs)
 }
+
+func TestCollectionSearchRange(t *testing.T) {
+	collectionName := createNewCollection(t, "companies")
+	documents := []interface{}{
+		newDocument("123", withCompanyName("Company 1"), withNumEmployees(50)),
+		newDocument("125", withCompanyName("Company 2"), withNumEmployees(150)),
+		newDocument("127", withCompanyName("Company 3"), withNumEmployees(250)),
+		newDocument("129", withCompanyName("Stark Industries 4"), withNumEmployees(500)),
+	}
+
+	params := &api.ImportDocumentsParams{Action: "create"}
+	_, err := typesenseClient.Collection(collectionName).Documents().Import(documents, params)
+	require.NoError(t, err)
+
+	searchParams := &api.SearchCollectionParams{
+		Q:        "*",
+		FilterBy: pointer.String("num_employees:>=100 && num_employees:<=300"),
+		SortBy:   &([]string{"num_employees:asc"}),
+		Page:     pointer.Int(1),
+		PerPage:  pointer.Int(10),
+	}
+
+	expectedDocs := []map[string]interface{}{
+		newDocumentResponse("125", withResponseCompanyName("Company 2"),
+			withResponseNumEmployees(150)),
+		newDocumentResponse("127", withResponseCompanyName("Company 3"),
+			withResponseNumEmployees(250)),
+	}
+
+	result, err := typesenseClient.Collection(collectionName).Documents().Search(searchParams)
+
+	require.NoError(t, err)
+	require.Equal(t, 2, result.Found, "found documents number is invalid")
+	require.Equal(t, 2, len(result.Hits), "number of hits is invalid")
+
+	docs := make([]map[string]interface{}, len(result.Hits))
+	for i, hit := range result.Hits {
+		docs[i] = hit.Document
+	}
+
+	require.Equal(t, expectedDocs, docs)
+}
