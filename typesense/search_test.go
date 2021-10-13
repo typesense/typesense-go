04a84132cd41900ff8fba033898ba6296095a6a5
Khalil Sarwari <khalil.sarwari@berkeley.edu>
2021-10-13 05:39:35 -0700

    Fix URL encoding for FilterBy (#77)
    * fix URL encoding of params
    
    * update search tests to include escaped params
    
    * use pointer lib
    
    * add range filter test from #63

--- typesense/search_test.go
+++ typesense/search_test.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"errors"
 	"net/http"
+	"net/url"
 	"testing"
 
 	"github.com/golang/mock/gomock"
@@ -112,6 +113,10 @@ func TestSearchResultDeserialization(t *testing.T) {
 
 func TestCollectionSearch(t *testing.T) {
 	expectedParams := newSearchParams()
+	expectedParams.Q = url.QueryEscape(expectedParams.Q)
+	if expectedParams.FilterBy != nil {
+		expectedParams.FilterBy = pointer.String(url.QueryEscape(*expectedParams.FilterBy))
+	}
 	expectedResult := newSearchResult()
 
 	ctrl := gomock.NewController(t)
@@ -136,6 +141,10 @@ func TestCollectionSearch(t *testing.T) {
 
 func TestCollectionSearchOnApiClientErrorReturnsError(t *testing.T) {
 	expectedParams := newSearchParams()
+	expectedParams.Q = url.QueryEscape(expectedParams.Q)
+	if expectedParams.FilterBy != nil {
+		expectedParams.FilterBy = pointer.String(url.QueryEscape(*expectedParams.FilterBy))
+	}
 
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
@@ -154,6 +163,10 @@ func TestCollectionSearchOnApiClientErrorReturnsError(t *testing.T) {
 
 func TestCollectionSearchOnHttpStatusErrorCodeReturnsError(t *testing.T) {
 	expectedParams := newSearchParams()
+	expectedParams.Q = url.QueryEscape(expectedParams.Q)
+	if expectedParams.FilterBy != nil {
+		expectedParams.FilterBy = pointer.String(url.QueryEscape(*expectedParams.FilterBy))
+	}
 
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
