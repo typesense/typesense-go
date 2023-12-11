26c55fd88538816819ef44afd50a6cbbeccaee7f
Kishore Nallan <kishorenc@gmail.com>
2023-12-11 21:02:49 +0530

    Add SearchGroupedHit.Found param

--- typesense/api/types_gen.go
+++ typesense/api/types_gen.go
@@ -474,6 +474,7 @@ type MultiSearchSearchesParameter struct {
 
 // SearchGroupedHit defines model for SearchGroupedHit.
 type SearchGroupedHit struct {
+	Found    *int          `json:"found,omitempty"`
 	GroupKey []interface{} `json:"group_key"`
 
 	// Hits The documents that matched the search query
