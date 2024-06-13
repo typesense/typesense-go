51ac6429f3dc76b5f96d7ec2c383f63dc5499a0f
Scott Talbot <s@chikachow.org>
2024-06-13 18:37:41 +1000

    Bump oapi-codegen to v2 (#165)

--- generator.sh
+++ generator.sh
@@ -1,2 +1,2 @@
-go run github.com/deepmap/oapi-codegen/cmd/oapi-codegen -package api -generate client -o $(pwd)/typesense/api/client_gen.go $(pwd)/typesense/api/generator/generator.yml
-go run github.com/deepmap/oapi-codegen/cmd/oapi-codegen -package api -generate types -o $(pwd)/typesense/api/types_gen.go $(pwd)/typesense/api/generator/generator.yml
+go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen -package api -generate client -o $(pwd)/typesense/api/client_gen.go $(pwd)/typesense/api/generator/generator.yml
+go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen -package api -generate types -o $(pwd)/typesense/api/types_gen.go $(pwd)/typesense/api/generator/generator.yml
