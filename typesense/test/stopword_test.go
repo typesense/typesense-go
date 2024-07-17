967576c9c7f11d3c0e40e5cec9a7ad3da94c21a5
Hayden <hayden.hhoang@gmail.com>
2024-07-17 18:14:09 +0700

    test-integration: retrieve & delete a stopword set

---
+++ typesense/test/stopword_test.go
@@ -0,0 +1,65 @@
+//go:build integration
+// +build integration
+
+package test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/typesense/typesense-go/typesense/api"
+	"github.com/typesense/typesense-go/typesense/api/pointer"
+)
+
+func TestStopwordRetrieve(t *testing.T) {
+	stopwordsSetID := newUUIDName("stopwordsSet-test")
+	upsertData := &api.StopwordsSetUpsertSchema{
+		Locale:    pointer.String("en"),
+		Stopwords: []string{"Germany", "France", "Italy", "United States"},
+	}
+
+	expectedData := &api.StopwordsSetSchema{
+		Id:        stopwordsSetID,
+		Locale:    upsertData.Locale,
+		Stopwords: upsertData.Stopwords,
+	}
+
+	result, err := typesenseClient.Stopwords().Upsert(context.Background(), stopwordsSetID, upsertData)
+
+	require.NoError(t, err)
+	require.Equal(t, expectedData, result)
+
+	result, err = typesenseClient.Stopword(stopwordsSetID).Retrieve(context.Background())
+
+	expectedData.Stopwords = []string{"states", "united", "france", "germany", "italy"}
+
+	require.NoError(t, err)
+	require.Equal(t, expectedData, result)
+}
+
+func TestStopwordDelete(t *testing.T) {
+	stopwordsSetID := newUUIDName("stopwordsSet-test")
+	upsertData := &api.StopwordsSetUpsertSchema{
+		Locale:    pointer.String("en"),
+		Stopwords: []string{"Germany", "France", "Italy", "United States"},
+	}
+
+	expectedData := &api.StopwordsSetSchema{
+		Id:        stopwordsSetID,
+		Locale:    upsertData.Locale,
+		Stopwords: upsertData.Stopwords,
+	}
+
+	result, err := typesenseClient.Stopwords().Upsert(context.Background(), stopwordsSetID, upsertData)
+	require.NoError(t, err)
+	require.Equal(t, expectedData, result)
+
+	result2, err := typesenseClient.Stopword(stopwordsSetID).Delete(context.Background())
+
+	require.NoError(t, err)
+	require.Equal(t, stopwordsSetID, result2.Id)
+
+	_, err = typesenseClient.Stopword(stopwordsSetID).Retrieve(context.Background())
+	require.Error(t, err)
+}
