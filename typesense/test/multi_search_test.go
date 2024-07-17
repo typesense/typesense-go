9cb18d207bd599ccc9cc10ebb0cb3927740c6e75
Hayden <hayden.hhoang@gmail.com>
2024-07-17 09:09:57 +0700

    test-integration: multi-search with preset

--- typesense/test/multi_search_test.go
+++ typesense/test/multi_search_test.go
@@ -191,3 +191,82 @@ func TestMultiSearchVectorQuery(t *testing.T) {
 	require.NotNil(t, searchResp.Results[0].Hits)
 	require.Equal(t, 1, len(*searchResp.Results[0].Hits))
 }
+
+func TestMultiSearchWithPreset(t *testing.T) {
+	collectionName1 := createNewCollection(t, "companies")
+	collectionName2 := createNewCollection(t, "companies")
+	documents := []interface{}{
+		newDocument("123", withCompanyName("Company 1"), withNumEmployees(50)),
+		newDocument("125", withCompanyName("Company 2"), withNumEmployees(150)),
+		newDocument("127", withCompanyName("Company 3"), withNumEmployees(250)),
+		newDocument("129", withCompanyName("Stark Industries 4"), withNumEmployees(500)),
+		newDocument("131", withCompanyName("Stark Industries 5"), withNumEmployees(1000)),
+	}
+
+	params := &api.ImportDocumentsParams{Action: pointer.String("create")}
+	_, err := typesenseClient.Collection(collectionName1).Documents().Import(context.Background(), documents, params)
+	require.NoError(t, err)
+
+	_, err = typesenseClient.Collection(collectionName1).Documents().Import(context.Background(), documents, params)
+	require.NoError(t, err)
+
+	searches := api.MultiSearchSearchesParameter{
+		Searches: []api.MultiSearchCollectionParameters{
+			{
+				Collection: collectionName1,
+				FilterBy:   pointer.String("num_employees:>100"),
+				SortBy:     pointer.String("num_employees:desc"),
+			},
+			{
+				Collection: collectionName1,
+				FilterBy:   pointer.String("num_employees:>1000"),
+			},
+			{
+				Collection: collectionName2,
+				Q:          pointer.String("Stark"),
+				FilterBy:   pointer.String("num_employees:>=1000"),
+			},
+		},
+	}
+
+	presetName := newUUIDName("preset-multi-search")
+	preset := &api.PresetUpsertSchema{}
+	preset.Value.FromMultiSearchSearchesParameter(searches)
+
+	_, err = typesenseClient.Presets().Upsert(context.Background(), presetName, preset)
+	require.NoError(t, err)
+
+	searchParams := &api.MultiSearchParams{
+		FilterBy: pointer.String("num_employees:>100"),
+		Q:        pointer.String("Company"),
+		QueryBy:  pointer.String("company_name"),
+		Preset:   &presetName,
+	}
+
+	expectedDocs1 := []map[string]interface{}{
+		newDocumentResponse("127", withResponseCompanyName("Company 3"), withResponseNumEmployees(250)),
+		newDocumentResponse("125", withResponseCompanyName("Company 2"), withResponseNumEmployees(150)),
+	}
+
+	expectedDocs2 := []map[string]interface{}{
+		newDocumentResponse("131", withResponseCompanyName("Stark Industries 5"), withResponseNumEmployees(1000)),
+	}
+
+	result, err := typesenseClient.MultiSearch.Perform(context.Background(), searchParams, api.MultiSearchSearchesParameter{})
+	require.NoError(t, err)
+
+	require.Equal(t, 3, len(result.Results))
+
+	// Check first result
+	for i, doc := range *result.Results[0].Hits {
+		require.Equal(t, *doc.Document, expectedDocs1[i])
+	}
+
+	// Check second result
+	require.Equal(t, 0, len(*result.Results[1].Hits))
+
+	// Check third result
+	for i, doc := range *result.Results[2].Hits {
+		require.Equal(t, *doc.Document, expectedDocs2[i])
+	}
+}
