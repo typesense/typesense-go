77daa7296b957f235ddae596e742b4c833f806f4
Fanis Tharropoulos <ftharropoulos@gmail.com>
2025-08-21 17:16:29 +0300

    feat(nl-models): add support for nl models

---
+++ typesense/nl_search_models.go
@@ -0,0 +1,45 @@
+package typesense
+
+import (
+	"context"
+
+	"github.com/typesense/typesense-go/v3/typesense/api"
+)
+
+type NLSearchModelsInterface interface {
+	Retrieve(ctx context.Context) ([]*api.NLSearchModelSchema, error)
+	Create(ctx context.Context, model *api.NLSearchModelCreateSchema) (*api.NLSearchModelSchema, error)
+}
+
+type nlSearchModels struct {
+	apiClient APIClientInterface
+}
+
+func (n *nlSearchModels) Retrieve(ctx context.Context) ([]*api.NLSearchModelSchema, error) {
+	response, err := n.apiClient.RetrieveAllNLSearchModelsWithResponse(ctx)
+	if err != nil {
+		return nil, err
+	}
+	if response.JSON200 == nil {
+		return nil, &HTTPError{Status: response.StatusCode(), Body: response.Body}
+	}
+
+	// Convert []NLSearchModelSchema to []*NLSearchModelSchema
+	result := make([]*api.NLSearchModelSchema, len(*response.JSON200))
+	for i, model := range *response.JSON200 {
+		modelCopy := model // Create a copy to get address
+		result[i] = &modelCopy
+	}
+	return result, nil
+}
+
+func (n *nlSearchModels) Create(ctx context.Context, model *api.NLSearchModelCreateSchema) (*api.NLSearchModelSchema, error) {
+	response, err := n.apiClient.CreateNLSearchModelWithResponse(ctx, *model)
+	if err != nil {
+		return nil, err
+	}
+	if response.JSON201 == nil {
+		return nil, &HTTPError{Status: response.StatusCode(), Body: response.Body}
+	}
+	return response.JSON201, nil
+}
