2c9a55a66a016cfeda2285fc11b86b443a7523a4
Fanis Tharropoulos <ftharropoulos@gmail.com>
2025-08-22 17:47:42 +0300

    refactor(test): refactor synonym set tests to nested format

--- typesense/test/synonym_set_test.go
+++ typesense/test/synonym_set_test.go
@@ -11,95 +11,92 @@ import (
 	"github.com/typesense/typesense-go/v3/typesense/api"
 )
 
-func TestSynonymSetRetrieve(t *testing.T) {
-	synonymSetName := newUUIDName("test-synonym-set")
-	synonymSetData := &api.SynonymSetCreateSchema{
-		Items: []api.SynonymItemSchema{
-			{
-				Id:       "dummy",
-				Synonyms: []string{"foo", "bar", "baz"},
-			},
-		},
+func synonymSetCleanUp() {
+	synonymSets, _ := typesenseClient.SynonymSets().Retrieve(context.Background())
+	for _, ss := range synonymSets {
+		typesenseClient.SynonymSet(ss.Name).Delete(context.Background())
 	}
-
-	// Create a synonym set first
-	_, err := typesenseClient.SynonymSets().Upsert(context.Background(), synonymSetName, synonymSetData)
-	require.NoError(t, err)
-
-	// Retrieve the specific synonym set
-	result, err := typesenseClient.SynonymSet(synonymSetName).Retrieve(context.Background())
-
-	require.NoError(t, err)
-	require.NotNil(t, result)
-	require.Equal(t, 1, len(result.Items))
-	require.Equal(t, "dummy", result.Items[0].Id)
-	require.Equal(t, []string{"foo", "bar", "baz"}, result.Items[0].Synonyms)
-
-	// Cleanup
-	_, err = typesenseClient.SynonymSet(synonymSetName).Delete(context.Background())
-	require.NoError(t, err)
 }
 
-func TestSynonymSetUpdate(t *testing.T) {
-	synonymSetName := newUUIDName("test-synonym-set")
-	originalData := &api.SynonymSetCreateSchema{
-		Items: []api.SynonymItemSchema{
-			{
-				Id:       "dummy",
-				Synonyms: []string{"foo", "bar"},
+func TestSynonymSet(t *testing.T) {
+	shouldSkipAnalyticsTests(t)
+	t.Cleanup(synonymSetCleanUp)
+
+	t.Run("Retrieve", func(t *testing.T) {
+		synonymSetName := newUUIDName("test-synonym-set")
+		synonymSetData := &api.SynonymSetCreateSchema{
+			Items: []api.SynonymItemSchema{
+				{
+					Id:       "dummy",
+					Synonyms: []string{"foo", "bar", "baz"},
+				},
 			},
-		},
-	}
+		}
 
-	// Create a synonym set first
-	_, err := typesenseClient.SynonymSets().Upsert(context.Background(), synonymSetName, originalData)
-	require.NoError(t, err)
+		_, err := typesenseClient.SynonymSets().Upsert(context.Background(), synonymSetName, synonymSetData)
+		require.NoError(t, err)
 
-	// Update the synonym set
-	updatedData := &api.SynonymSetCreateSchema{
-		Items: []api.SynonymItemSchema{
-			{
-				Id:       "dummy",
-				Synonyms: []string{"foo", "bar", "baz", "qux"},
+		result, err := typesenseClient.SynonymSet(synonymSetName).Retrieve(context.Background())
+
+		require.NoError(t, err)
+		require.NotNil(t, result)
+		require.Equal(t, 1, len(result.Items))
+		require.Equal(t, "dummy", result.Items[0].Id)
+		require.Equal(t, []string{"foo", "bar", "baz"}, result.Items[0].Synonyms)
+	})
+
+	t.Run("Update", func(t *testing.T) {
+		synonymSetName := newUUIDName("test-synonym-set")
+		originalData := &api.SynonymSetCreateSchema{
+			Items: []api.SynonymItemSchema{
+				{
+					Id:       "dummy",
+					Synonyms: []string{"foo", "bar"},
+				},
 			},
-		},
-	}
+		}
 
-	result, err := typesenseClient.SynonymSet(synonymSetName).Upsert(context.Background(), updatedData)
+		_, err := typesenseClient.SynonymSets().Upsert(context.Background(), synonymSetName, originalData)
+		require.NoError(t, err)
 
-	require.NoError(t, err)
-	require.Equal(t, synonymSetName, result.Name)
-	require.Equal(t, 1, len(result.Items))
-	require.Equal(t, "dummy", result.Items[0].Id)
-	require.Equal(t, []string{"foo", "bar", "baz", "qux"}, result.Items[0].Synonyms)
-
-	// Cleanup
-	_, err = typesenseClient.SynonymSet(synonymSetName).Delete(context.Background())
-	require.NoError(t, err)
-}
-
-func TestSynonymSetDelete(t *testing.T) {
-	synonymSetName := newUUIDName("test-synonym-set")
-	synonymSetData := &api.SynonymSetCreateSchema{
-		Items: []api.SynonymItemSchema{
-			{
-				Id:       "dummy",
-				Synonyms: []string{"foo", "bar", "baz"},
+		updatedData := &api.SynonymSetCreateSchema{
+			Items: []api.SynonymItemSchema{
+				{
+					Id:       "dummy",
+					Synonyms: []string{"foo", "bar", "baz", "qux"},
+				},
 			},
-		},
-	}
+		}
 
-	// Create a synonym set first
-	_, err := typesenseClient.SynonymSets().Upsert(context.Background(), synonymSetName, synonymSetData)
-	require.NoError(t, err)
+		result, err := typesenseClient.SynonymSet(synonymSetName).Upsert(context.Background(), updatedData)
 
-	// Delete the synonym set
-	result, err := typesenseClient.SynonymSet(synonymSetName).Delete(context.Background())
+		require.NoError(t, err)
+		require.Equal(t, synonymSetName, result.Name)
+		require.Equal(t, 1, len(result.Items))
+		require.Equal(t, "dummy", result.Items[0].Id)
+		require.Equal(t, []string{"foo", "bar", "baz", "qux"}, result.Items[0].Synonyms)
+	})
 
-	require.NoError(t, err)
-	require.Equal(t, synonymSetName, result.Name)
+	t.Run("Delete", func(t *testing.T) {
+		synonymSetName := newUUIDName("test-synonym-set")
+		synonymSetData := &api.SynonymSetCreateSchema{
+			Items: []api.SynonymItemSchema{
+				{
+					Id:       "dummy",
+					Synonyms: []string{"foo", "bar", "baz"},
+				},
+			},
+		}
 
-	// Verify it's deleted by trying to retrieve it
-	_, err = typesenseClient.SynonymSet(synonymSetName).Retrieve(context.Background())
-	require.Error(t, err)
+		_, err := typesenseClient.SynonymSets().Upsert(context.Background(), synonymSetName, synonymSetData)
+		require.NoError(t, err)
+
+		result, err := typesenseClient.SynonymSet(synonymSetName).Delete(context.Background())
+
+		require.NoError(t, err)
+		require.Equal(t, synonymSetName, result.Name)
+
+		_, err = typesenseClient.SynonymSet(synonymSetName).Retrieve(context.Background())
+		require.Error(t, err)
+	})
 } 
--- typesense/test/synonym_sets_test.go
+++ typesense/test/synonym_sets_test.go
@@ -11,68 +11,69 @@ import (
 	"github.com/typesense/typesense-go/v3/typesense/api"
 )
 
-func TestSynonymSetsUpsert(t *testing.T) {
-	synonymSetName := newUUIDName("test-synonym-set")
-	synonymSetData := &api.SynonymSetCreateSchema{
-		Items: []api.SynonymItemSchema{
-			{
-				Id:       "dummy",
-				Synonyms: []string{"foo", "bar", "baz"},
-			},
-		},
+func synonymSetsCleanUp() {
+	synonymSets, _ := typesenseClient.SynonymSets().Retrieve(context.Background())
+	for _, ss := range synonymSets {
+		typesenseClient.SynonymSet(ss.Name).Delete(context.Background())
 	}
-
-	result, err := typesenseClient.SynonymSets().Upsert(context.Background(), synonymSetName, synonymSetData)
-
-	require.NoError(t, err)
-	require.Equal(t, synonymSetName, result.Name)
-	require.Equal(t, 1, len(result.Items))
-	require.Equal(t, "dummy", result.Items[0].Id)
-	require.Equal(t, []string{"foo", "bar", "baz"}, result.Items[0].Synonyms)
-
-	// Cleanup
-	_, err = typesenseClient.SynonymSet(synonymSetName).Delete(context.Background())
-	require.NoError(t, err)
 }
 
-func TestSynonymSetsRetrieve(t *testing.T) {
-	synonymSetName := newUUIDName("test-synonym-set")
-	synonymSetData := &api.SynonymSetCreateSchema{
-		Items: []api.SynonymItemSchema{
-			{
-				Id:       "dummy",
-				Synonyms: []string{"foo", "bar", "baz"},
+func TestSynonymSets(t *testing.T) {
+	shouldSkipAnalyticsTests(t)
+	t.Cleanup(synonymSetsCleanUp)
+
+	t.Run("Upsert", func(t *testing.T) {
+		synonymSetName := newUUIDName("test-synonym-set")
+		synonymSetData := &api.SynonymSetCreateSchema{
+			Items: []api.SynonymItemSchema{
+				{
+					Id:       "dummy",
+					Synonyms: []string{"foo", "bar", "baz"},
+				},
 			},
-		},
-	}
-
-	// Create a synonym set first
-	_, err := typesenseClient.SynonymSets().Upsert(context.Background(), synonymSetName, synonymSetData)
-	require.NoError(t, err)
-
-	// Retrieve all synonym sets
-	result, err := typesenseClient.SynonymSets().Retrieve(context.Background())
-
-	require.NoError(t, err)
-	require.NotNil(t, result)
-	require.GreaterOrEqual(t, len(result), 1)
-
-	// Find our created synonym set
-	var foundSynonymSet *api.SynonymSetSchema
-	for _, ss := range result {
-		if ss.Name == synonymSetName {
-			foundSynonymSet = &ss
-			break
 		}
-	}
 
-	require.NotNil(t, foundSynonymSet)
-	require.Equal(t, synonymSetName, foundSynonymSet.Name)
-	require.Equal(t, 1, len(foundSynonymSet.Items))
-	require.Equal(t, "dummy", foundSynonymSet.Items[0].Id)
-	require.Equal(t, []string{"foo", "bar", "baz"}, foundSynonymSet.Items[0].Synonyms)
+		result, err := typesenseClient.SynonymSets().Upsert(context.Background(), synonymSetName, synonymSetData)
 
-	// Cleanup
-	_, err = typesenseClient.SynonymSet(synonymSetName).Delete(context.Background())
-	require.NoError(t, err)
+		require.NoError(t, err)
+		require.Equal(t, synonymSetName, result.Name)
+		require.Equal(t, 1, len(result.Items))
+		require.Equal(t, "dummy", result.Items[0].Id)
+		require.Equal(t, []string{"foo", "bar", "baz"}, result.Items[0].Synonyms)
+	})
+
+	t.Run("Retrieve", func(t *testing.T) {
+		synonymSetName := newUUIDName("test-synonym-set")
+		synonymSetData := &api.SynonymSetCreateSchema{
+			Items: []api.SynonymItemSchema{
+				{
+					Id:       "dummy",
+					Synonyms: []string{"foo", "bar", "baz"},
+				},
+			},
+		}
+
+		_, err := typesenseClient.SynonymSets().Upsert(context.Background(), synonymSetName, synonymSetData)
+		require.NoError(t, err)
+
+		result, err := typesenseClient.SynonymSets().Retrieve(context.Background())
+
+		require.NoError(t, err)
+		require.NotNil(t, result)
+		require.GreaterOrEqual(t, len(result), 1)
+
+		var foundSynonymSet *api.SynonymSetSchema
+		for _, ss := range result {
+			if ss.Name == synonymSetName {
+				foundSynonymSet = &ss
+				break
+			}
+		}
+
+		require.NotNil(t, foundSynonymSet)
+		require.Equal(t, synonymSetName, foundSynonymSet.Name)
+		require.Equal(t, 1, len(foundSynonymSet.Items))
+		require.Equal(t, "dummy", foundSynonymSet.Items[0].Id)
+		require.Equal(t, []string{"foo", "bar", "baz"}, foundSynonymSet.Items[0].Synonyms)
+	})
 } 
