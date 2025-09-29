74b800c022a8e7e693751321b92d9bf833679227
Fanis Tharropoulos <ftharropoulos@gmail.com>
2025-09-29 19:37:23 +0300

    feat(curation): add new curation sets api

---
+++ typesense/curation_set.go
@@ -0,0 +1,56 @@
+package typesense
+
+import (
+	"context"
+
+	"github.com/typesense/typesense-go/v3/typesense/api"
+)
+
+// CurationSetInterface is a type for individual Curation Set API operations
+type CurationSetInterface interface {
+	// Retrieve a single curation set
+	Retrieve(ctx context.Context) (*api.CurationSetRetrieveSchema, error)
+	// Update a curation set
+	Upsert(ctx context.Context, curationSetSchema *api.CurationSetCreateSchema) (*api.CurationSetSchema, error)
+	// Delete a curation set
+	Delete(ctx context.Context) (*api.CurationSetDeleteSchema, error)
+}
+
+// curationSet is internal implementation of CurationSetInterface
+type curationSet struct {
+	apiClient       APIClientInterface
+	curationSetName string
+}
+
+func (c *curationSet) Retrieve(ctx context.Context) (*api.CurationSetRetrieveSchema, error) {
+	response, err := c.apiClient.RetrieveCurationSetWithResponse(ctx, c.curationSetName)
+	if err != nil {
+		return nil, err
+	}
+	if response.JSON200 == nil {
+		return nil, &HTTPError{Status: response.StatusCode(), Body: response.Body}
+	}
+	return response.JSON200, nil
+}
+
+func (c *curationSet) Upsert(ctx context.Context, curationSetSchema *api.CurationSetCreateSchema) (*api.CurationSetSchema, error) {
+	response, err := c.apiClient.UpsertCurationSetWithResponse(ctx, c.curationSetName, api.UpsertCurationSetJSONRequestBody(*curationSetSchema))
+	if err != nil {
+		return nil, err
+	}
+	if response.JSON200 == nil {
+		return nil, &HTTPError{Status: response.StatusCode(), Body: response.Body}
+	}
+	return response.JSON200, nil
+}
+
+func (c *curationSet) Delete(ctx context.Context) (*api.CurationSetDeleteSchema, error) {
+	response, err := c.apiClient.DeleteCurationSetWithResponse(ctx, c.curationSetName)
+	if err != nil {
+		return nil, err
+	}
+	if response.JSON200 == nil {
+		return nil, &HTTPError{Status: response.StatusCode(), Body: response.Body}
+	}
+	return response.JSON200, nil
+}
---
+++ typesense/curation_sets.go
@@ -0,0 +1,49 @@
+package typesense
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"github.com/typesense/typesense-go/v3/typesense/api"
+)
+
+// CurationSetsInterface is a type for Curation Sets API operations
+type CurationSetsInterface interface {
+	// Create or update a curation set
+	Upsert(ctx context.Context, curationSetName string, curationSetSchema *api.CurationSetCreateSchema) (*api.CurationSetSchema, error)
+	// Retrieve all curation sets
+	Retrieve(ctx context.Context) ([]api.CurationSetSchema, error)
+}
+
+// curationSets is internal implementation of CurationSetsInterface
+type curationSets struct {
+	apiClient APIClientInterface
+}
+
+func (c *curationSets) Upsert(ctx context.Context, curationSetName string, curationSetSchema *api.CurationSetCreateSchema) (*api.CurationSetSchema, error) {
+	response, err := c.apiClient.UpsertCurationSetWithResponse(ctx, curationSetName, api.UpsertCurationSetJSONRequestBody(*curationSetSchema))
+	if err != nil {
+		return nil, err
+	}
+	if response.JSON200 == nil {
+		return nil, &HTTPError{Status: response.StatusCode(), Body: response.Body}
+	}
+	return response.JSON200, nil
+}
+
+func (c *curationSets) Retrieve(ctx context.Context) ([]api.CurationSetSchema, error) {
+	response, err := c.apiClient.RetrieveCurationSetsWithResponse(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	// The API returns an array directly as specified in the OpenAPI spec
+	var curationSets []api.CurationSetSchema
+	if err := json.Unmarshal(response.Body, &curationSets); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal curation sets response: %w", err)
+	}
+
+	return curationSets, nil
+}
+
